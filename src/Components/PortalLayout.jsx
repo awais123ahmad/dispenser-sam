6a6d7102d0e8b36b79cc6ea0379554cfb9c740dd
usman5432 <usmaniking@gmail.com>
2024-12-04 14:42:35 +0500

    dispenser cleaned

--- src/Components/PortalLayout.jsx
+++ src/Components/PortalLayout.jsx
@@ -9,40 +9,22 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../assets/logo.png";
 
-import { FaFirstAid, FaUserInjured } from 'react-icons/fa';
-
 import { Popover, Transition } from "@headlessui/react";
 
 import { useLocation, useNavigate } from "react-router-dom";
 import { IoIosArrowDown } from "react-icons/io";
-import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
 import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
-import BadgeIcon from "@mui/icons-material/Badge";
-import CategoryIcon from "@mui/icons-material/Category";
 import Inventory2Icon from "@mui/icons-material/Inventory2";
-import WarehouseIcon from "@mui/icons-material/Warehouse";
-import WindowIcon from "@mui/icons-material/Window";
-import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion";
-import SquareFootIcon from "@mui/icons-material/SquareFoot";
-import AspectRatioIcon from "@mui/icons-material/AspectRatio";
-import WidgetsIcon from "@mui/icons-material/Widgets";
-
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import CleanHandsIcon from "@mui/icons-material/CleanHands";
-import PaidIcon from "@mui/icons-material/Paid";
-import ReceiptIcon from "@mui/icons-material/Receipt";
-import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
-import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+
 import {
   SESSION_IS_AUTHENTICATED,
   SESSION_USERINFO,
 } from "../Utills/Constants";
 //import { useDispatch } from "react-redux";
-import { logout } from "../Api/Reducers/authReducer";
-import { MedicalInformation, PeopleAltRounded } from "@mui/icons-material";
+
 
 const drawerWidth = 240;
 
@@ -280,7 +262,7 @@ export default function PortalLayout({ children }) {
 
                 <ul className={`w-[100%]`}>
                  
-                  <li>
+                  {/* <li>
                     <div
                       onClick={() => route("/dashboard")}
                       className={`flex items-center p-2 cursor-pointer  text-gray-600 mt-3 h-[2.6rem] ${
@@ -312,7 +294,7 @@ export default function PortalLayout({ children }) {
                         Dashboard
                       </span>
                     </div>
-                  </li>
+                  </li> */}
 
                   <li>
                     <div
--- src/Services/loginService.js
+++ src/Services/loginService.js
@@ -3,7 +3,7 @@ import axiosInstance from "./axiosInstance";
 const loginService = {
     fetchAll: async () => {
         try {
-            const response = await axiosInstance.get('/dispensers/all');
+            const response = await axiosInstance.get('/dispenser/all');
             return response.data;
         } catch (error) {
             throw error.response?.data || error.message;
@@ -12,7 +12,7 @@ const loginService = {
 
     fetchById: async (id) => {
         try {
-            const response = await axiosInstance.get(`/dispensers/id/${id}`);
+            const response = await axiosInstance.get(`/dispense/id/${id}`);
             return response.data;
         } catch (error) {
             throw error.response?.data || error.message;
@@ -21,7 +21,7 @@ const loginService = {
 
     create: async (payload) => {
         try {
-            const response = await axiosInstance.post('/dispensers/register', payload);
+            const response = await axiosInstance.post('/dispenser/register', payload);
             return response.data;
         } catch (error) {
             throw error.response?.data || error.message;
@@ -30,7 +30,7 @@ const loginService = {
 
     update: async (id, payload) => {
         try {
-            const response = await axiosInstance.put(`/dispensers/edit/${id}`, payload);
+            const response = await axiosInstance.put(`/dispenser/edit/${id}`, payload);
             return response.data;
         } catch (error) {
             throw error.response?.data || error.message;
@@ -39,7 +39,7 @@ const loginService = {
 
     delete: async (id) => {
         try {
-            const response = await axiosInstance.delete(`/dispensers/delete/${id}`);
+            const response = await axiosInstance.delete(`/dispenser/delete/${id}`);
             return response.data;
         } catch (error) {
             throw error.response?.data || error.message;
@@ -48,7 +48,7 @@ const loginService = {
 
     login: async (payload) => {
         try {
-            const response = await axiosInstance.post(`/dispensers/login`, payload);
+            const response = await axiosInstance.post(`/dispenser/login`, payload);
             return response.data;
         } catch (error) {
             throw error.response?.data || error.message;
